5ea596eadf768d4bb0d2029467f68e85cf5b90e4
hugo <hugo.tienza@estudiantat.fib.upc.edu>
2024-11-17 17:54:29 +0100

    begin

---
+++ backend/pae/src/test/java/com/pae/pae/PaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pae.pae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
