1992fe62c364c885af9de3d7fd8ab69d232bd122
joan Llonch <joan.llonch@estudiantat.upc.edu>
2024-12-07 20:21:15 +0100

    jwt funcionant?

--- backend/pae/src/main/java/com/pae/pae/controllers/UsuariController.java
+++ backend/pae/src/main/java/com/pae/pae/controllers/UsuariController.java
@@ -1,24 +1,13 @@
 package com.pae.pae.controllers;
 
-import com.pae.pae.models.Jornada;
-import com.pae.pae.models.Rols;
 import com.pae.pae.models.UsuariDTO;
-import com.pae.pae.repositories.UsuariRepository;
 import com.pae.pae.services.UsuariService;
-import com.pae.pae.utils.JWTUtil;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
 import java.util.Map;
 
 @RestController
