d3bc76ab20a34679f5a4e0f5cf4672a1be295fdb
hugo <hugo.tienza@estudiantat.fib.upc.edu>
2025-01-04 12:56:49 +0100

    add param Date in algorithm

--- backend/pae/src/main/java/com/pae/pae/controllers/AlgorithmController.java
+++ backend/pae/src/main/java/com/pae/pae/controllers/AlgorithmController.java
@@ -3,12 +3,10 @@ package com.pae.pae.controllers;
 import com.pae.pae.services.AlgorithmService;
 import com.pae.pae.services.TecnicService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/algorithm")
@@ -17,8 +15,8 @@ public class AlgorithmController {
     private AlgorithmService algorithmService = new AlgorithmService();
 
     @CrossOrigin
-    @GetMapping
-    public boolean execute() throws SQLException {
-        return algorithmService.execute();
+    @GetMapping(path = "/{date}")
+    public boolean execute(@PathVariable("date") Date date) throws SQLException {
+        return algorithmService.execute(date);
     }
 }
