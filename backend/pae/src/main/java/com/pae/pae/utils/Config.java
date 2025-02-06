02bef6dc60a65f8fb14dbd878acb5ab4692aeda3
Oriol Vilella <oriol.vilella@estudiantat.upc.edu>
2025-02-06 15:49:55 +0100

    error config

--- backend/pae/src/main/java/com/pae/pae/utils/Config.java
+++ backend/pae/src/main/java/com/pae/pae/utils/Config.java
@@ -3,7 +3,7 @@ package com.pae.pae.utils;
 public class Config {
 
     //public final static String URL = System.getenv("DB_URL");
-    public final static String URL = "jdbc:postgresql://10.4.41.33:5432/pae";
+    public final static String URL = "jdbc:postgresql://127.0.0.1:5432/pae";
     //public final static String USER = System.getenv("DB_USERNAME");
     public final static String USER = "postgres";
     //public final static String PWD = System.getenv("DB_PASSWORD");
